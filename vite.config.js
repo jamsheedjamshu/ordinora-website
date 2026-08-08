d4bd8e6f4edcca599e24be10cf1d5cd16fd86690
jamsheedjamshu <m.jamshu@gmail.com>
2026-08-08 12:27:12 +0800

    Update Packages/Pricing webpage

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         services: resolve(__dirname, 'services.html'),
         about: resolve(__dirname, 'about.html'),
-        contact: resolve(__dirname, 'contact.html')
+        contact: resolve(__dirname, 'contact.html'),
+        packages: resolve(__dirname, 'packages.html')
       }
     }
   },
